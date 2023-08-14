b8427021360b207f1295ebac32e4b106a1b99a48
zuliadin <zuliadin.mur@gmail.com>
2023-08-15 02:58:25 +0700

    add copy junit

--- app/src/test/java/com/onevour/sdk/impl/ExampleUnitTest.java
+++ app/src/test/java/com/onevour/sdk/impl/ExampleUnitTest.java
@@ -9,18 +9,26 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.*;
 
+import com.onevour.core.utilities.beans.BeanCopy;
+import com.onevour.sdk.impl.repositories.models.Employee;
+import com.onevour.sdk.impl.repositories.models.Person;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({Log.class})
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest({Log.class})
 public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+        Employee employee = new Employee("1", "John Doe");
+        Person person = BeanCopy.value(employee, Person.class);
+        assertEquals("John Doe", person.getName());
+        assertEquals(0, person.getId());
     }
 
 }
