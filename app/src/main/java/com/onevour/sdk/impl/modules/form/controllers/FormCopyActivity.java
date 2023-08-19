f66f1301b8fff411b6a71bf1da001d5e33591298
zuliadin <zuliadin.mur@gmail.com>
2023-08-19 19:22:17 +0700

    add chaced bean copy field

---
+++ app/src/main/java/com/onevour/sdk/impl/modules/form/controllers/FormCopyActivity.java
@@ -0,0 +1,16 @@
+package com.onevour.sdk.impl.modules.form.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.onevour.sdk.impl.R;
+
+public class FormCopyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_copy);
+    }
+}
